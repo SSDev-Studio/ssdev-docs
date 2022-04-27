d8f1579cadccb466bef3a2a0daff7758244d746a
Alex Redding <Alexredding180@gmail.com>
2022-04-27 22:28:14 +0100

    Initial

---
+++ docs/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 0
+---
+
+# Introduction
+
+## Who are we?
+We are a small group of developers working to create resources for FiveM!
---
+++ docusaurus.config.js
@@ -0,0 +1,100 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/nightOwlLight');
+const darkCodeTheme = require('prism-react-renderer/themes/nightOwl');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'SSDev Documentation',
+  tagline: 'SSDev Resource Documentation',
+  url: 'https://http://docs.ssdev.studio',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'https://github.com/SSDev-Studio', // Usually your GitHub org/user name.
+  projectName: 'ssdev-docs', // Usually your repo name.
+  trailingSlash: false,
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/SSDev-Studio/ssdev-docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'SSDev Docs',
+        logo: {
+          alt: 'SSDev Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/SSDev-Studio/ssdev-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/fp5daz53UZ',
+              }
+            ],
+          },
+          {
+            title: 'Github',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/SSDev-Studio',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} SSDev Studios, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
